b38a8ce17a486ff411dce2133ff1bbfb5d05b8f3
olliekemp <103338687+olliekemp@users.noreply.github.com>
2023-06-07 14:13:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     description='Functions to train and compress neural networks',
     author='Ollie Kemp',
     packages = setuptools.find_packages(),
-    install_requires=requirements
+    install_requires=requirements,
+    python_requires='>=3.10',
 )
